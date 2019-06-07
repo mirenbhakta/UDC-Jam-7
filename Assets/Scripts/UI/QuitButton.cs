99f0391ed2b7377fda6f467c6e838b996d217b53
Miren <mirenbhakta21@gmail.com>
2019-06-06 19:30:31 -0500

    exit button in WebGL will just go back to the main page

--- Assets/Scripts/UI/QuitButton.cs
+++ Assets/Scripts/UI/QuitButton.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
 
 public class QuitButton : MonoBehaviour
 {
+#if UNITY_WEBGL
+
+    [DllImport("__Internal")]
+    private static extern void QuitWebGL();
+
+    public void Quit()
+    {
+        QuitWebGL();
+    }
+
+#else
+
     public void Quit()
     {
         Application.Quit();
     }
+
+#endif
 }
